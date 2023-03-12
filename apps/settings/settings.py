3acf3db6b18205dcda7a1255020e962f8922e5ab
Alex Karachanidi <alexkarachanidi@Alexs-MacBook-Pro.local>
2023-03-12 17:57:27 +0600

    added separation of settings for different environments

--- apps/settings/settings.py
+++ apps/settings/settings.py
@@ -75,21 +75,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'apps.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': '5432',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
